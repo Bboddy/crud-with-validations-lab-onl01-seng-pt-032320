e2c4928e1fa72eae4139fd05189d10b4e2c95105
Bryce B <bryce.boddy@gmail.com>
2020-08-24 13:48:41 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -18,7 +18,7 @@ class Song < ActiveRecord::Base
     if release_year && release_year >= year
       #binding.pry
       errors.add(:release_year, "Must be less than or equal to the current year")
-    elsif released && re == nil 
+    elsif released && release_year == nil 
       
       errors.add(:title, "Must not be blank if released")
     end
